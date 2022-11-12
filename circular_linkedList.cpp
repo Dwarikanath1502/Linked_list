527f96aebbbdad1f66885e189a5ef74387cf9969
Dwarikanath1502 <ddwarikanath15@gmail.com>
2022-11-12 20:01:14 +0530

    detect loop in LL, find start of loop, delete loop

---
+++ circular_linkedList.cpp
@@ -0,0 +1,86 @@
+// TODO:    CIRCULAR LINKED LIST and check is it circular
+
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+};
+
+void push(Node **head_ref, int data)
+{
+    Node *new_node = new Node();
+    new_node->data = data;
+    new_node->next = (*head_ref);
+    Node *temp = (*head_ref);
+
+    if ((*head_ref) != NULL)
+    {
+        while (temp->next != (*head_ref))
+        {
+            temp = temp->next;
+        }
+        temp->next = new_node;
+    }
+    else
+    {
+        new_node->next = new_node;
+    }
+    (*head_ref) = new_node;
+}
+
+
+
+bool isCircular(Node *head)
+{
+    if (head== NULL)
+    {
+        return 1;
+    }
+    Node *temp = head->next;
+    while (temp != NULL && temp != head)
+    {
+        temp = temp->next;
+    }
+    if (temp == NULL)
+    {
+        return 0;
+    }
+    if (temp == head)
+    {
+        return 1;
+    }
+}
+
+void printList(Node *head)
+{
+    Node *temp = head;
+    do
+    {
+        cout << head->data << " ";
+        head = head->next;
+    } while (temp != head);
+}
+
+
+
+int main()
+{
+    Node *head = NULL;
+    push(&head, 10);
+    push(&head, 20);
+    push(&head, 30);
+    push(&head, 40);
+    push(&head, 50);
+    printList(head);
+    if (isCircular(head))
+    {
+        cout<<"\nit is circular"<<endl;
+    }else{
+        cout<<"\nNot circular"<<endl;
+    }  
+    return 0;
+}
---
+++ findLoop.cpp
@@ -0,0 +1,113 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+};
+
+void push(Node **head_ref, int data)
+{
+    Node *new_node = new Node();
+    new_node->data = data;
+    new_node->next = (*head_ref);
+    (*head_ref) = new_node;
+}
+// TODO: find loop
+Node *findLoop(Node *head)
+{
+    if (head == NULL)
+    {
+        return NULL;
+    }
+    Node *slow = head;
+    Node *fast = head;
+    while (slow != NULL && fast != NULL)
+    {
+        slow = slow->next;
+        fast = fast->next;
+        if (fast != NULL)
+        {
+            fast = fast->next;
+        }
+        if (slow == fast)
+        {
+            // cout<<"Loop is present at: "<<slow->data;
+            return slow;
+        }
+    }
+    return NULL;
+}
+// TODO: find starting node of loop
+Node *findStart(Node *head)
+{
+    if (head == NULL)
+    {
+        return NULL;
+    }
+
+    Node *intersection = findLoop(head);
+    Node *slow = head;
+    while (slow != intersection)
+    {
+        slow = slow->next;
+        intersection = intersection->next;
+    }
+    return slow;
+}
+// TODO: remove loop
+Node *removeLoop(Node *head)
+{
+    if (head == NULL)
+    {
+        return NULL;
+    }
+    Node *startOfLoop = findStart(head);
+    Node *temp = startOfLoop;
+    while (temp != startOfLoop)
+    {
+        temp = temp->next;
+    }
+    temp->next = NULL;
+}
+void print(Node *head)
+{
+    while (head != NULL)
+    {
+        cout << head->data << " ";
+        head = head->next;
+    }
+}
+
+int main()
+{
+    Node *head = NULL;
+    push(&head, 10);
+    push(&head, 20);
+    push(&head, 30);
+    push(&head, 40);
+    print(head);
+    head->next->next->next = head->next;
+    if (findLoop(head) != NULL)
+    {
+        cout << "\nLoop is present" << endl;
+    }
+    else
+    {
+        cout << "Loop is not present" << endl;
+    }
+    cout << "starting at: " << findStart(head)->data << endl;
+    removeLoop(head);
+     if (findLoop(head) != NULL)
+    {
+        cout << "\nLoop is present" << endl;
+    }
+    else
+    {
+        cout << "Loop is not present" << endl;
+    }
+    cout << "starting at: " << findStart(head)->data << endl;
+    return 0;
+}
