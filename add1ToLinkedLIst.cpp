80eff003363446fcf94cf65adc12822d1e4106d3
Dwarikanath1502 <ddwarikanath15@gmail.com>
2022-11-17 19:13:48 +0530

    add 1 to the link list

---
+++ add1ToLinkedLIst.cpp
@@ -0,0 +1,92 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+};
+
+void push(Node **head_ref, int data)
+{
+    Node *new_node = new Node();
+    new_node->data = data;
+    new_node->next = (*head_ref);
+    (*head_ref) = new_node;
+}
+// TODO: REVERSE THE LINK LIST
+Node *reverseList(Node *head)
+{
+    if (head == NULL)
+    {
+        return 0;
+    }
+    Node *current = head;
+    Node *prev = NULL;
+    Node *next = NULL;
+
+    while (current != NULL)
+    {
+        next = current->next;
+        current->next = prev;
+        prev = current;
+        current = next;
+    }
+    head = prev;
+}
+// TODO: ADD 1 TO THE LINK LIST
+Node *addOne(Node *head)
+{
+    head = reverseList(head);
+    int carry = 1;
+    Node *current = head;
+    while (carry)
+    {
+        current->data += 1;
+        if (current->data < 10)
+        {
+            return reverseList(head);
+        }
+        else
+        {
+            current->data = 0;
+        }
+        if (current->next == NULL)
+        {
+            return 0;
+        }
+        else
+        {
+            current = current->next;
+        }
+    }
+}
+
+
+void printList(Node *head)
+{
+    while (head != NULL)
+    {
+        cout << head->data << " ";
+        head = head->next;
+    }
+}
+
+int main()
+{
+    Node *head = NULL;
+    push(&head, 1);
+    push(&head, 0);
+    push(&head, 9);
+    printList(head);
+    head = reverseList(head);
+    cout << endl
+         << "reversed list..." << endl;
+    printList(head);
+    head = addOne(head);
+    cout << endl
+         << "Result after adding One: " << endl;
+    printList(head);
+    return 0;
+}
