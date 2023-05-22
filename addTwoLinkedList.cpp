18ef2d19c8762fb13c6728094e4d950023664d9f
Dwarikanath1502 <ddwarikanath15@gmail.com>
2023-05-22 23:49:48 +0530

    adding two LL

--- addTwoLinkedList.cpp
+++ addTwoLinkedList.cpp
@@ -1,92 +1,45 @@
-// TODO: ADD TEO NUMBERS REPRESENTED BY LINKED LIST
-// FIXME:
-#include <iostream>
+// Add two liniked list
+
+#include<iostream>
 using namespace std;
 
-class Node
-{
-public:
+class Node{
+    public: 
     int data;
-    Node *next;
+    Node* next;
 };
 
-void push(Node **head_ref, int data)
-{
-    Node *new_node = new Node();
+void insert(Node** head_ref, int data){
+    Node* new_node = new Node();
     new_node->data = data;
     new_node->next = (*head_ref);
     (*head_ref) = new_node;
 }
 
-Node *reverse(Node *head)
-{
-    if (head == NULL)
+void printList(Node* head){
+    while (head!=NULL)
     {
-        return 0;
-    }
-    Node *current = head;
-    Node *prev = NULL;
-    Node *next = NULL;
-    while (current != NULL)
-    {
-        next = current->next;
-        current->next = prev;
-
-        prev = current;
-        current = next;
-    }
-    head = prev;
-}
-
-Node *add(Node *first, Node *second)
-{
-    int carry = 0;
-    while (first != NULL && second != NULL)
-    {
-        int sum = carry + first->data + second->data;
-    }
-}
-
-Node *addList(Node *first, Node *second)
-{
-    // step 1: reverse input of linked list
-    first = reverse(first);
-    second = reverse(second);
-    // step 2: add two linked list
-    Node *ans = add(first, second);
-    // step 3: reverse ans to get final ans
-    ans = reverse(ans);
-    return ans;
-}
-
-void printList(Node *head)
-{
-    while (head != NULL)
-    {
-        cout << head->data << " ";
+        cout<<head->data<<" ";
         head = head->next;
-    }
+    }    
 }
+
+
+
+
 
 int main()
 {
-    Node *head1 = NULL;
-    push(&head1, 1);
-    push(&head1, 2);
-    push(&head1, 3);
-    cout << "Node 1: " << endl;
-    printList(head1);
-
-    Node *head2 = NULL;
-    push(&head2, 1);
-    push(&head2, 2);
-    push(&head2, 3);
-    cout << endl
-         << "Node 2: " << endl;
+    Node *head = NULL;
+    insert(&head, 5);
+    insert(&head, 4);
+    cout<<"List 1: "<<endl;
+    printList(head);
+    Node* head2 = NULL;
+    insert(&head2, 3);
+    insert(&head2, 4);
+    insert(&head2, 5);
+    cout<<endl<<"List 2: "<<endl;
     printList(head2);
-    cout <<endl<< "SUM: " << endl;
-    Node *sum = NULL;
-    sum = addList(head1, head2);
-    printList(sum);
     return 0;
 }
