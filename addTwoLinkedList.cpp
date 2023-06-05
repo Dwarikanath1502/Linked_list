161b0c237ed5adc76c883d0ef1609e55f3ed4d32
Dwarikanath1502 <ddwarikanath15@gmail.com>
2023-06-05 10:55:38 +0530

    code working on gfg practice

--- addTwoLinkedList.cpp
+++ addTwoLinkedList.cpp
@@ -1,45 +1,117 @@
 // Add two liniked list
 
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-class Node{
-    public: 
+class Node
+{
+public:
     int data;
-    Node* next;
+    Node *next;
+    Node(int x)
+    {
+        data = x;
+        next = NULL;
+    }
 };
 
-void insert(Node** head_ref, int data){
-    Node* new_node = new Node();
-    new_node->data = data;
-    new_node->next = (*head_ref);
-    (*head_ref) = new_node;
-}
+// void insert(Node **head_ref, int data)
+// {
+//     Node *new_node = new Node();
+//     new_node->data = data;
+//     new_node->next = (*head_ref);
+//     (*head_ref) = new_node;
+// }
 
-void printList(Node* head){
-    while (head!=NULL)
+Node *reverse(Node *head)
+{
+
+    Node *current = head;
+    Node *next = NULL;
+    Node *prev = NULL;
+    while (current != NULL)
     {
-        cout<<head->data<<" ";
-        head = head->next;
-    }    
+        next = current->next;
+        current->next = prev;
+
+        prev = current;
+        current = next;
+    }
+    head = prev;
+    return head;
 }
 
+Node *addList(Node *first, Node *second)
+{
+    // step 1:
+    first = reverse(first);
+    second = reverse(second);
+    // step 2:
+    int sum = 0;
+    int carry = 0;
+    Node *temp;
+    Node *result = NULL;
+    Node *current = NULL;
+    // iterate to list
+    while (first != NULL || second != NULL)
+    {
+        sum = carry + (first ? first->data : 0) + (second ? second->data : 0);
+        carry = (sum >= 10) ? 1 : 0;
+        sum = (sum % 10);
+        temp = new Node(sum);
+        if (result == NULL)
+        {
+            result = temp;
+        }
+        else
+        {
+            current->next = temp;
+        }
+        current = temp;
+        // if element remains
+        if (first)
+        {
+            first = first->next;
+        }
+        if (second)
+        {
+            second = second->next;
+        }
+    }
+    // if there is a carry remain
+    if (carry > 0)
+    {
+        temp = new Node(carry);
+        current->next = temp;
+        current = temp;
+    }
+    result = reverse(result);
+    return result;
+}
 
-
-
+void printList(Node *head)
+{
+    while (head != NULL)
+    {
+        cout << head->data << " ";
+        head = head->next;
+    }
+}
 
 int main()
 {
     Node *head = NULL;
     insert(&head, 5);
     insert(&head, 4);
-    cout<<"List 1: "<<endl;
-    printList(head);
-    Node* head2 = NULL;
+
+    head = reverse(head);
+
+    Node *head2 = NULL;
     insert(&head2, 3);
     insert(&head2, 4);
     insert(&head2, 5);
-    cout<<endl<<"List 2: "<<endl;
-    printList(head2);
+    head2 = reverse(head2);
+    Node *ans = addList(head, head2);
+    printList(ans);
     return 0;
 }
