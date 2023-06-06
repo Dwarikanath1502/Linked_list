723ca1ad7ef570276f27d75dea32ef02e3cc86d1
Dwarikanath1502 <ddwarikanath15@gmail.com>
2023-06-06 23:24:28 +0530

    find intersection of linked list

--- intersection.cpp
+++ intersection.cpp
@@ -1,3 +1,15 @@
+// TODO: find the intersection of two inked list
+
+/*
+APPROACH
+    push all the elements of head1 to set, then find all the elements of head2 in set till end
+    if it matches then store the head2->data to result vector and increment count od index and
+     head = head->next
+
+     when head2 gets null return ans
+
+*/
+
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -16,31 +28,31 @@ void push(Node **head_ref, int data)
     (*head_ref) = new_node;
 }
 
-Node *intersection(Node *head1, Node *head2)
+vector<int> intersection(Node *head1, Node *head2, int k)
 {
-    Node *first = head1;
-    Node *second = head2;
-    while (first != second)
+    vector<int> res(k);
+    unordered_set<int> set;
+
+    while (head1 != NULL)
     {
-        if (first == NULL)
-        {
-            first = head2;
-        }
-        else
-        {
-            first = first->next;
-        }
-        if (second = NULL)
-        {
-            second = head1;
-        }
-        else
-        {
-            second = second->next;
-        }
+        set.insert(head1->data);
+        head1 = head1->next;
     }
-    return first;
+
+    // to keep track of element in res array
+    int count = 0;
+    while (head2 != NULL)
+    {
+        if (set.find(head2->data) != set.end())
+        {
+            res[count] = head2->data;
+            count++;
+        }
+        head2 = head2->next;
+    }
+    return res;
 }
+
 void printList(Node *head)
 {
     while (head != NULL)
@@ -65,7 +77,11 @@ int main()
     push(&second, 21);
     push(&second, 7);
     Node *ans = NULL;
-    ans = intersection(first, second);
-    printList(ans);
+    vector<int> arr = intersection(first, second, 2);
+    for (auto i : arr)
+    {
+        cout << i << " ";
+    }
+
     return 0;
 }
