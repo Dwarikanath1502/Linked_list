a4f7484d358268b49ba5a14f7393bddbdad0041f
Dwarikanath1502 <ddwarikanath15@gmail.com>
2023-06-09 23:47:17 +0530

    flattening linked list using recursion, updated code with all comments

--- flattenLL.cpp
+++ flattenLL.cpp
@@ -1,3 +1,42 @@
+/*
+TODO: Given a linked list where every node represents a linked list and contains two pointers of its type:
+
+Pointer to next node in the main list (we call it ‘right’ pointer in the code below)
+Pointer to a linked list where this node is headed (we call it the ‘down’ pointer in the code below).
+Note: All linked lists are sorted and the resultant linked list should also be sorted
+
+Examples:
+
+Input:    5 -> 10 -> 19 -> 28
+               |        |         |        |
+              V       V       V       V
+              7      20      22     35
+               |                 |        |
+              V               V       V
+              8               50     40
+              |                          |
+             V                        V
+            30                       45
+
+
+Output: 5->7->8->10->19->20->22->28->30->35->40->45->50
+
+Input:    3 -> 10 -> 7 -> 14
+               |        |         |        |
+              V       V       V       V
+              9      47      15     22
+               |                 |
+              V                V
+              17              30
+
+Output: 3->7->9->10->14->15->17->22->30->47
+
+// APPROACH:
+    compare two linked from from last using recusion and merge then and so on until all lists are merged
+    TimeComplexity: as we are traversing through all the nodes so summation of all the nodes will be time complexity O(summation of all nodes)
+    SpaceComplexity: O(1)
+*/
+
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -26,9 +65,9 @@ Node *mergeTwoList(Node *first, Node *second)
     {
         return first;
     }
-
     Node *ans = new Node(-1);
     Node *temp = ans;
+
     while (first != NULL && second != NULL)
     {
         if (first->data < second->data)
@@ -51,22 +90,24 @@ Node *mergeTwoList(Node *first, Node *second)
     {
         temp->bottom = second;
     }
-    return ans->bottom;
+    ans = ans->bottom;
+    return ans;
 }
 
 // method to flatten linked list
 Node *flatten(Node *root)
 {
-    // base case
+    // base case: if there is no node then return null else root->next element is NULL then return node to prev
     if (root == NULL || root->next == NULL)
     {
         return root;
     }
-    // recursion to right
+    // right recursion for merging the elemnt
     root->next = flatten(root->next);
 
+    // merge root and and root->next element
     root = mergeTwoList(root, root->next);
-
+    // return root
     return root;
 }
 
@@ -74,7 +115,7 @@ void printList(Node *head)
 {
     while (head != NULL)
     {
-        cout << head->data<<" ";
+        cout << head->data << " ";
         head = head->bottom;
     }
 }
