2f9a07db498630e7433e2fe66a6bd6611577841c
Dwarikanath1502 <ddwarikanath15@gmail.com>
2023-08-05 15:41:37 +0530

    remove duplicates from unsorted linked list

--- removeDuplicateUnsorted.cpp
+++ removeDuplicateUnsorted.cpp
@@ -1,3 +1,19 @@
+/*
+TODO: remove duplicates from unsorted likedlist
+
+TC: O(N)
+SC:O(N)
+
+APPROACH:
+    if no element in the list return NULL, else push the first element in an unordered_set
+    loop throught the list while current && current->next is not null and compare whather current->next->data
+    is present in the list or not
+    * if present -> make current->next = current->next->next
+    * if not present push current->next->data to the list and move current = current->next;
+
+
+*/
+
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -18,27 +34,36 @@ void push(Node **head_ref, int value)
 
 /*TODO: It will take time complexity of O(n2)*/
 
-// Node *removeDuplicate(Node *head)
-// {
-//     Node *current = head;
-//     while (current != NULL)
-//     {
-//         Node *temp = current->next;
-//         while (temp != NULL)
-//         {
-//             if (current->data == temp->data)
-//             {
-//                 current->next = temp->next;
-//                 free(temp);
-//                 removeDuplicate(current);
-//             }
-//             else
-//             {
-//                 removeDuplicate(current->next);
-//             }
-//         }
-//     }
-// }
+Node *removeDuplicate(Node *head)
+{
+    unordered_set<int> seen;
+    Node *current = head;
+
+    if (current == NULL)
+    {
+        return head;
+    }
+    else
+    {
+        seen.insert(current->data);
+    }
+
+    while (current != NULL && current->next != NULL)
+    {
+        // same
+        if (seen.find(current->next->data) != seen.end())
+        {
+            current->next = current->next->next;
+        }
+        else
+        {
+            // not same
+            seen.insert(current->next->data);
+            current = current->next;
+        }
+    }
+    return head;
+}
 
 void printList(Node *head)
 {
@@ -51,6 +76,21 @@ void printList(Node *head)
 
 int main()
 {
-
+    Node *head = NULL;
+    push(&head, 1);
+    push(&head, 5);
+    push(&head, 15);
+    push(&head, 5);
+    push(&head, 15);
+    push(&head, 12);
+    push(&head, 13);
+    push(&head, 14);
+    push(&head, 1);
+    cout << "List before removal of duplicates: ";
+    printList(head);
+    cout << endl
+         << "List after removal of duplicates: ";
+    head = removeDuplicate(head);
+    printList(head);
     return 0;
 }
